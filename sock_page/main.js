d13ab4b960a0a5e817df9b14d22ea26ebf3acfa3
Blergborg <pprater92@gmail.com>
2023-04-28 07:31:34 -0700

    Added dynamic list rendering. Added key setting for dynamic list elements.

--- sock_page/main.js
+++ sock_page/main.js
@@ -13,7 +13,11 @@ const app = Vue.createApp({
             // inStock: false,
             inventory: 10,
             onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
         }
     }
 })
