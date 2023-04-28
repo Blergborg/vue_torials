d3fad01560c46bc2158b04229661834e4c0b1029
Blergborg <pprater92@gmail.com>
2023-04-27 23:11:36 -0700

    Added notes about Vue. Instantiated an app and mounted it to the DOM in index.html.

--- sock_page/main.js
+++ sock_page/main.js
@@ -1,1 +1,13 @@
-const product = "Socks"
+// Call Vue.createApp(), pass it an obj w/ a "data" property that's a function.
+// data() returns an obj version of the previous const product = "Socks"
+
+// The obj we passs to Vue.createApp() is called the "options object". Must always pass in at least an empty obj.
+// Vue.createApp() makes a Vue instance, which powers everything else.
+const app = Vue.createApp({
+    data() {        // this line shorthad for ( data: function() { )
+        return {
+            product: "Socks",
+            description: "Stop sniffing them. Don't be weird."
+        }
+    }
+})
