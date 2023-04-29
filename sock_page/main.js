175b353ef36d199bdec0b9e444cd9534c8858e2b
Blergborg <pprater92@gmail.com>
2023-04-29 09:44:48 -0700

    Added conditional styling to "Add to Card" button
    Added enable/disable functionality to "Add to Card" button

--- sock_page/main.js
+++ sock_page/main.js
@@ -19,6 +19,11 @@ const app = Vue.createApp({
                 {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
                 {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
+            // can also add styles object(s) w/in data property to prevent messy inline styling
+            styles: {
+                color: 'red',
+                fontSize: '14px',
+            }
         }
     },
     methods: {
