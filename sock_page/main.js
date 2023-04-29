4b67eb6633efa26dacbe8946e1ef2911ea0db61a
Blergborg <pprater92@gmail.com>
2023-04-29 08:58:41 -0700

    used v-on to attach methods from vue app. attachced to click and mouseover listenters.

--- sock_page/main.js
+++ sock_page/main.js
@@ -16,9 +16,17 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
             ],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1  // "this.cart" references cart in our data.
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
