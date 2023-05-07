7a55164cf5b7a23531b94bd7a310555e7bb78054
Blergborg <pprater92@gmail.com>
2023-05-06 18:00:53 -0700

    basic scaffolding

--- real-world-vue/src/main.js
+++ real-world-vue/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'  // root component, in which all other components are nested. All app code is in here.
+import App from './App.vue' // root component, in which all other components are nested. All app code is in here.
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)  // actually create the app.
+const app = createApp(App) // actually create the app.
 
 // tell the app to use Pinia and router.
 app.use(createPinia())
